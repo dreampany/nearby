3ab5a4b5803f2ca8f566f861b8c7ad8f40aa08f1
Roman <hawladar.roman@bjitgroup.com>
2019-12-20 19:22:32 +0600

    2nd commit

---
+++ lib/splash/splash_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+/**
+ * Created by roman on 2019-12-20
+ * Copyright (c) 2019 bjit. All rights reserved.
+ * hawladar.roman@bjitgroup.com
+ * Last modified $file.lastModified
+ */
+
+class SplashScreen extends StatefulWidget {
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+
+  var visible = true;
+  AnimationController controller;
+  Animation<double> animation;
+
+  @override
+  Widget build(BuildContext context) {
+     return null;
+  }
+
+}
